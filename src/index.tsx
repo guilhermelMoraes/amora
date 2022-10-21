f229f933ae612e542295f94c4062e5a3c5ab5a78
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-10-21 12:34:19 -0300

    feat: add material ui lib

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 import Router from './common/router';
